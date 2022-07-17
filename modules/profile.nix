ebe2c5adc8ed97530f63f53eaeee19e7715d216d
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-07-17 12:47:06 +0200

    also set stateversion in profile (?)

--- modules/profile.nix
+++ modules/profile.nix
@@ -13,6 +13,8 @@
   ];
 
   boot.consoleLogLevel = lib.mkDefault 7;
+  
+  system.stateVersion = "22.05"; # Did you read the comment?
 
   boot.supportedFilesystems = lib.mkForce [ "btrfs" "vfat" "cifs" ];
 
